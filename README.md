fcf9deef985c7131b4db730f240004d4aac6c6bc
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-05-05 11:40:07 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Node.js, Express, MongoDB & More: The Complete Bootcamp 2021
+
+## Jonas Schmedtmann (Udemy)
+    - Web Developer, Designer, and Teacher
+    - link: https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/
+
+### Seção 6 & 7: Projeto Todo-app
+
+Aplicação para agencia de turismo. 
+
+ - Nodejs
+ - Express
+ - MongoDB
+ - Mongoose
+ - JWT
+ - AWS
---
+++ controllers/userController.js
@@ -0,0 +1,73 @@
+const User = require('./../models/userModel')
+const catchAsync = require('./../utils/catchAsync')
+
+const appError = require('./../utils/appError')
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {}
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el]
+    })
+    return newObj
+}
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    const users = await User.find()
+
+    // Send response
+    res.status(200).json({
+        status: 'success',
+        resultes: users.length,
+        data: {
+            users
+        }
+    })
+})
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    // 1) Create erro if user POSTs password data
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(new appError('This route is not for password updates. Please use /updateMyPassword', 400))
+    }
+
+    // 2) Filtered out unwanted fields names that are not allowed to be updated 
+    const filteredBody = filterObj(req.body, 'name', 'email')
+
+    // 3) Update user document
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {new: true, runValidators: true})
+     
+    res.status(200).json({
+        status: 'success',
+        data : {
+            user: updatedUser
+        }
+    })
+})
+
+exports.getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined!'
+    })
+}
+
+exports.createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined!'
+    })
+}
+
+exports.updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined!'
+    })
+}
+
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined!'
+    })
+}
